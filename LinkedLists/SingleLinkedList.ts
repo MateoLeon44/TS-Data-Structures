4d98201da17420566828af72bffe634cca6f6e33
MateoLeonAlzate <m.leona@uniandes.edu.co>
2022-10-21 19:11:03 -0500

    Set getters and setters

--- LinkedLists/SingleLinkedList.ts
+++ LinkedLists/SingleLinkedList.ts
@@ -1,4 +1,22 @@
-export class SingleLinkedList {
-  constructor(public head: LinkedListNode | null = null, public size: number = 0) {
+import { LinkedListNode } from './LinkedListNode'
+
+export class SingleLinkedList<T> {
+  constructor (public _head: LinkedListNode<T> | null = null, public _size: number = 0) {
+  }
+
+  get head (): LinkedListNode<T> | null {
+    return this._head
+  }
+
+  set head (headParam:LinkedListNode<T> | null) {
+    this._head = headParam
+  }
+
+  get size (): number {
+    return this.size
+  }
+
+  set size (sizeParam: number) {
+    this._size = sizeParam
   }
 }
