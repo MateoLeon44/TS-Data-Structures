42472332a21acf4c6c04e652032b633f2c395c45
MateoLeonAlzate <m.leona@uniandes.edu.co>
2022-10-21 19:52:00 -0500

    Made next default value to be null

--- LinkedLists/LinkedListNode.ts
+++ LinkedLists/LinkedListNode.ts
@@ -1,3 +1,3 @@
 export class LinkedListNode<T> {
-  constructor(public data: T | null = null, public next: LinkedListNode<T> | null) {}
+  constructor(public data: T | null = null, public next: LinkedListNode<T> | null = null) {}
 }
